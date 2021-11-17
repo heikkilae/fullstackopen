50fea039a2fb2758106d9d2afa3e7495802c08f6
Antti Heikkilä <antti.heikkilae@hotmail.com>
2021-11-17 16:47:21 +0200

    osa1-1.5: kurssitiedot step5

--- osa1/1.1-1.2/src/App.js
+++ osa1/1.1-1.2/src/App.js
@@ -27,34 +27,36 @@ const Content = (props) => {
 const Total = (props) => {
   const parts = props.parts
   const exercises = parts[0].exercises + parts[1].exercises + parts[2].exercises
-  
+
   return (
     <p>Number of exercises {exercises}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
