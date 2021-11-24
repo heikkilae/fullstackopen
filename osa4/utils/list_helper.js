ea3c6693b52eca0a88eb71d37fb1be2e10197dba
Antti Heikkilä <antti.heikkilae@hotmail.com>
2021-11-24 19:09:17 +0200

    osa4-4.4: apufunktioita ja yksikkötestejä, step2

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = blogs => {
+  const likes = blogs.map(blog => blog.likes)
+  const reducer = (previous, next) => previous + next
+  return likes.reduce(reducer)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
