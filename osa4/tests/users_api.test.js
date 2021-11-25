809edd20c7f885fed7726f9bd245a9971e5c62c6
Antti Heikkilä <antti.heikkilae@hotmail.com>
2021-11-25 20:18:51 +0200

    osa4-4.17: blogilistan laajennus, step5

--- osa4/tests/users_api.test.js
+++ osa4/tests/users_api.test.js
@@ -12,7 +12,7 @@ describe('when there is initially one user at db', () => {
     await User.deleteMany({})
 
     const passwordHash = await bcrypt.hash('sekret', 10)
-    const user = new User({ username: 'root', passwordHash })
+    const user = new User({ username: 'root', name: 'Super User', passwordHash })
 
     await user.save()
   })
