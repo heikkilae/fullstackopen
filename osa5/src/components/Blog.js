d68dfb22548732dd74f5357e9b787af202c7fb1f
Antti Heikkilä <antti.heikkilae@hotmail.com>
2021-11-27 17:13:31 +0200

    osa5-5.7* blogilistan frontend, step7

--- osa5/src/components/Blog.js
+++ osa5/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const buttonLabel = !visible ? 'view' : 'hide'
+  const likes = blog.likes ? blog.likes : 0
+
+  const showWhenVisibleStyle = {
+    display: visible ? '' : 'none'
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={toggleVisibility}>{buttonLabel}</button>
+      <div style={showWhenVisibleStyle}>
+        {blog.url} <br />
+        {likes} <br />
+        {blog.user.name}
+      </div>
+    </div>
+  )}
 
 export default Blog
