54b0ee9a741ef2d5d36f605409153d1918935b8f
Antti Heikkilä <antti.heikkilae@hotmail.com>
2021-11-27 14:55:58 +0200

    osa5-5.11: blogilistan frontend, step11

--- osa5/src/components/Login.js
+++ osa5/src/components/Login.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Login = ({ username, password, handleUsername, handlePassword, handleLogin }) => {
+const Login = ({ 
+  username, 
+  password, 
+  handleUsername, 
+  handlePassword, 
+  handleLogin 
+}) => {
   return (
     <div>
     <h2>log in to application</h2>
@@ -27,6 +34,14 @@ const Login = ({ username, password, handleUsername, handlePassword, handleLogin
     </form>
   </div>
   )
+}
+
+Login.propTypes = {
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  handleUsername: PropTypes.func.isRequired,
+  handlePassword: PropTypes.func.isRequired,
+  handleLogin: PropTypes.func.isRequired,
 }
 
 export default Login
