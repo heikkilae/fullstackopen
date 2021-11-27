7e18407ebc774391c6c6c4dabe00ed5bdf47f1b3
Antti Heikkilä <antti.heikkilae@hotmail.com>
2021-11-27 11:39:37 +0200

    osa4-4.18, 4.19: Fix user id

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -22,7 +22,7 @@ blogsRouter.post('/', async (request, response) => {
   if (!token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
