358cd2f26c2e5f2b7b0f565b808c1f0441aeee0e
Antti Heikkilä <antti.heikkilae@hotmail.com>
2021-11-27 22:48:24 +0200

    osa5-5.14: blogilistan testit, step2

--- osa5/src/components/Blog.test.js
+++ osa5/src/components/Blog.test.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    title: 'Title text',
-    author: 'Jorkki'
-  }
+const blog = {
+  title: 'Title text',
+  author: 'Jorkki',
+  url: 'testurl',
+  likes: 666
+}
 
+test('renders content', () => {
   const component = render(
     <Blog blog={blog} />
   )
@@ -18,4 +20,21 @@ test('renders content', () => {
   )
 
   expect(element).toBeDefined()
+})
+
+test('clicking the view button shows url and likes', async () => {
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent(
+    'testurl'
+  )
+
+  expect(component.container).toHaveTextContent(
+    '666'
+  )
 })
