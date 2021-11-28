be17de8e2bfb2f05b43e91e94aaa835f8c0bd721
Antti Heikkilä <antti.heikkilae@hotmail.com>
2021-11-28 12:47:13 +0200

    osa5-5.18: blogilistan end to end -testit, step2

--- osa5/cypress/integration/blog_app.spec.js
+++ osa5/cypress/integration/blog_app.spec.js
@@ -1,11 +1,34 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Teppo Testaaja',
+      username: 'tTestaaja',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.visit('http://localhost:3000')
     cy.contains('log in to application')
+  })
+
+  describe('Login',function() {
+    it('login succeeds with correct credentials', function() {
+      cy.get('#username').type('tTestaaja')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+      
+      cy.contains('tTestaaja logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('vaaraMies')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+      cy.contains('log in to application')
+    })
   })
 })
