034d7ecf6050bb8aa223c55c0accdf8c62707e58
Antti Heikkilä <antti.heikkilae@hotmail.com>
2021-11-28 13:08:25 +0200

    5.19: blogilistan end to end -testit, step3

--- osa5/cypress/integration/blog_app.spec.js
+++ osa5/cypress/integration/blog_app.spec.js
@@ -1,12 +1,19 @@
+const testUser = {
+  name: 'Teppo Testaaja',
+  username: 'tTestaaja',
+  password: 'salainen'
+}
+
+function login(username, password) {
+  cy.get('#username').type(username)
+  cy.get('#password').type(password)
+  cy.get('#login-button').click()
+}
+
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    const user = {
-      name: 'Teppo Testaaja',
-      username: 'tTestaaja',
-      password: 'salainen'
-    }
-    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.request('POST', 'http://localhost:3003/api/users/', testUser)
     cy.visit('http://localhost:3000')
   })
 
@@ -17,18 +24,31 @@ describe('Blog app', function() {
 
   describe('Login',function() {
     it('login succeeds with correct credentials', function() {
-      cy.get('#username').type('tTestaaja')
-      cy.get('#password').type('salainen')
-      cy.get('#login-button').click()
-      
+      login('tTestaaja', 'salainen')
       cy.contains('tTestaaja logged in')
     })
 
     it('fails with wrong credentials', function() {
-      cy.get('#username').type('vaaraMies')
-      cy.get('#password').type('salainen')
-      cy.get('#login-button').click()
+      login('vaaraMies', 'salainen')
       cy.contains('log in to application')
     })
+  })
+})
+
+describe('When logged in', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users/', testUser)
+    cy.visit('http://localhost:3000')
+    login('tTestaaja', 'salainen')
+  })
+
+  it('A blog can be created', function() {
+    cy.contains('create new blog').click()
+    cy.get('#title').type('The very first blog post')
+    cy.get('#author').type('Teppo Testaaja')
+    cy.get('#url').type('http://localhost')
+    cy.get('#create-button').click()
+    cy.contains('The very first blog post')
   })
 })
--- osa5/src/components/BlogForm.js
+++ osa5/src/components/BlogForm.js
@@ -36,16 +36,16 @@ const BlogForm = ({ handleSubmit }) => {
       <h2>create new</h2>
       <form>
         <div>
-          title: <input value={newTitle} onChange={handleNewTitle} />
+          title: <input id='title' value={newTitle} onChange={handleNewTitle} />
         </div>
         <div>
-          author: <input value={newAuthor} onChange={handleNewAuthor} />
+          author: <input id='author' value={newAuthor} onChange={handleNewAuthor} />
         </div>
         <div>
-          url: <input value={newUrl} onChange={handleNewUrl} />
+          url: <input id='url' value={newUrl} onChange={handleNewUrl} />
         </div>
         <div>
-          <button type="submit" onClick={create}>create</button>
+          <button id='create-button' type="submit" onClick={create}>create</button>
         </div>
       </form>
     </div>
