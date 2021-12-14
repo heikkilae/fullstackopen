2ae307d723b8dbf377a52cd93d6127ed5bd5256f
Antti Heikkilä <antti.heikkilae@hotmail.com>
2021-12-14 19:45:24 +0200

    osa8-8.8: Kirjailijoiden näkymä

---
+++ osa8/library-frontend/src/components/Authors.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../queries' 
+
+const Authors = (props) => {
+  const [authors, setAuthors] = useState(null)
+  const result = useQuery(ALL_AUTHORS)
+
+  useEffect(() => {
+    if (result.data && result.data.allAuthors !== null) {
+      setAuthors(result.data.allAuthors)
+    } else {
+      setAuthors([])
+    }
+  }, [result.data])
+  
+  if (!props.show || !authors) {
+    return null
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
