6b0dcb8564e6b8d03630a87c481e69a35216fa6a
Antti Heikkilä <antti.heikkilae@hotmail.com>
2021-12-19 15:28:31 +0200

    osa9-9.2 Exercise calculator

---
+++ osa9/exercises-1-7/exerciseCalculator.ts
@@ -0,0 +1,43 @@
+interface Stats {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string,
+    target: number;
+    average: number;
+}
+
+interface Rating {
+    grade: number;
+    explaining: string;
+}
+
+const calculateExercises = (hours: Array<number>): Stats => {
+    const rating = (average: number, target: number): Rating => {
+        console.log(average, target)
+        if (average > target) return { grade: 3, explaining: 'sick awesome!' };
+        if (average < target) return { grade: 1, explaining: 'you can do better' };
+        if (average === target) return { grade: 2, explaining: 'not too bad but could be better' };
+        return { grade: 0, explaining: '' };
+    }
+    
+    const target = 2
+    const totalHours = hours.reduce((p, c) => p + c)
+    const average = totalHours / hours.length
+    const ratingObj = rating(Math.ceil(average), target)
+    
+    const ret = {
+        periodLength: hours.length,
+        trainingDays: 5,
+        average: average,
+        target: target,
+        success: average >= target,
+        rating: ratingObj.grade,
+        ratingDescription: ratingObj.explaining
+    }
+
+    return ret
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1]))
